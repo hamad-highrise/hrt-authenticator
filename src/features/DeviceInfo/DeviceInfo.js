1dc8c747b2651ceb7a1123a431f3487b8ad83f97
RummanKhanHRT <rumman@highrisetechnologies.com>
2021-02-24 23:39:16 +0500

    biometricEdit & DeviceInfo views added, EmptyState desgined

---
+++ src/features/DeviceInfo/DeviceInfo.js
@@ -0,0 +1,279 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet,TouchableOpacity, Image, FlatList,Alert,Modal, TouchableHighlight} from 'react-native';
+import { TopNavbar } from '../../components';
+import  {Navigation} from 'react-native-navigation';
+
+const DeviceInfo = (props) => {
+    const onPressHandler = () => {
+        Navigation.push(props.componentId, {
+            component: {
+                name: 'authenticator.MainScreen',
+                options: {
+                    topBar: {
+                        visible: false
+                    }
+                }
+            }
+        });
+    };
+    // NOIFICATION SCREEEN WORK
+    const AccountConnected = () => {
+        Navigation.push(props.componentId, {
+            component: {
+                name: 'authenticator.NotifyAccountConnected',
+                options: {
+                    topBar: {
+                        visible: false
+                    }
+                }
+            }
+        });
+    };
+    const ProcessComplete = () => {
+        Navigation.push(props.componentId, {
+            component: {
+                name: 'authenticator.NotifyProcessComplete',
+                options: {
+                    topBar: {
+                        visible: false
+                    }
+                }
+            }
+        });
+    };
+    const BiometricOption = () => {
+        Navigation.push(props.componentId, {
+            component: {
+                name: 'authenticator.BiometricOption',
+                options: {
+                    topBar: {
+                        visible: false
+                    }
+                }
+            }
+        });
+    };
+    const onPressHandlerAccessCode = (id, name, issuer, secret) => {
+        Navigation.push(props.componentId, {
+            component: {
+                name: 'authenticator.AccessCodeScreen',
+                passProps: {
+                    id: id,
+                    name: name,
+                    issuer: issuer,
+                    secret: secret
+                },
+                options: {
+                    topBar: {
+                        visible: false
+                    }
+                }
+            }
+        });
+    };
+    const onPressHandlerAccountSettings = () => {
+        Navigation.push(props.componentId, {
+            component: {
+                name: 'authenticator.AccountSettingsScreen',
+                passProps: {
+                    id: props.id,
+                    refresh: props.refresh
+                },
+                options: {
+                    topBar: {
+                        visible: false
+                    }
+                }
+            }
+        });
+    };
+    const onPressHandlerSplashScreen = () => {
+        Navigation.push(props.componentId, {
+            component: {
+                name: 'authenticator.SplashScreen',
+                options: {
+                    topBar: {
+                        visible: false
+                    }
+                }
+            }
+        });
+    };
+    const onPressHandlerEmptyState = () => {
+        Navigation.push(props.componentId, {
+            component: {
+                name: 'authenticator.EmptyStateScreen',
+                options: {
+                    topBar: {
+                        visible: false
+                    }
+                }
+            }
+        });
+    };
+    const onPressHandlerError = () => {
+        Navigation.push(props.componentId, {
+            component: {
+                name: 'authenticator.NotifyError',
+                options: {
+                    topBar: {
+                        visible: false
+                    }
+                }
+            }
+        });
+    };
+    const onPressHandlerGetStarted = () => {
+        Navigation.push(props.componentId, {
+            component: {
+                name: 'authenticator.GetStarted',
+                options: {
+                    topBar: {
+                        visible: false
+                    }
+                }
+            }
+        });
+    };
+    return (
+        
+        <View style={styles.container}>
+            <TopNavbar title="Device Info" onPress={()=>alert("zxcvb")}></TopNavbar>
+
+            <View style={{ margin: 30}} />
+            
+            {/* li */}
+            <TouchableOpacity style={styles.listitem} onPress={AccountConnected}>
+                <View style={styles.listitemView}>
+                    <Text style={styles.listitemText}>{/*Version*/}Notify Account Connected</Text>
+                    <Image
+                        source={require('../../assets/icons/backarrowinvert.png')}
+                        style={styles.img}
+                    />
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.listitem} onPress={ProcessComplete}>
+                <View style={styles.listitemView}>
+                    <Text style={styles.listitemText}>{/*IBM Security Verify SDK version*/}Notify Process Complete</Text>
+                    <Image
+                        source={require('../../assets/icons/backarrowinvert.png')}
+                        style={styles.img}
+                    />
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.listitem} onPress={BiometricOption}>
+                <View style={styles.listitemView}>
+                    <Text style={styles.listitemText}>{/*IBM Security Verify User Guide*/}Biometric Option</Text>
+                    <Image
+                        source={require('../../assets/icons/backarrowinvert.png')}
+                        style={styles.img}
+                    />
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.listitem} onPress={onPressHandlerAccessCode}>
+                <View style={styles.listitemView}>
+                    <Text style={styles.listitemText}>{/*Remove this record*/}Access Code</Text>
+                    <Image
+                        source={require('../../assets/icons/backarrowinvert.png')}
+                        style={styles.img}
+                    />
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.listitem} onPress={onPressHandlerError}>
+                <View style={styles.listitemView}>
+                    <Text style={styles.listitemText}>{/*Terms and Conditions*/}Notify Error</Text>
+                    <Image
+                        source={require('../../assets/icons/backarrowinvert.png')}
+                        style={styles.img}
+                    />
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.listitem} onPress={onPressHandlerAccountSettings}>
+                <View style={styles.listitemView}>
+                    <Text style={styles.listitemText}>{/*Privacy Policy*/}Account Settings</Text>
+                    <Image
+                        source={require('../../assets/icons/backarrowinvert.png')}
+                        style={styles.img}
+                    />
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.listitem} onPress={onPressHandlerSplashScreen}>
+                <View style={styles.listitemView}>
+                    <Text style={styles.listitemText}>{/*Third Party Notices*/}SplashScreen</Text>
+                    <Image
+                        source={require('../../assets/icons/backarrowinvert.png')}
+                        style={styles.img}
+                    />
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.listitem} onPress={onPressHandlerEmptyState}>
+                <View style={styles.listitemView}>
+                    <Text style={styles.listitemText}>{/*Security Assessment*/}EmptyState</Text>
+                    <Image
+                        source={require('../../assets/icons/backarrowinvert.png')}
+                        style={styles.img}
+                    />
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.listitem} onPress={onPressHandlerGetStarted}>
+                <View style={styles.listitemView}>
+                    <Text style={styles.listitemText}>GetStarted</Text>
+                    <Image
+                        source={require('../../assets/icons/backarrowinvert.png')}
+                        style={styles.img}
+                    />
+                </View>
+            </TouchableOpacity>
+            {/* end li */}
+     
+        </View>
+    );
+};
+
+DeviceInfo.options = {};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    title: {
+        marginLeft: 20
+    },
+    
+    titleText: {
+        color: 'black',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+
+    listitem: {
+        padding: 15,
+        backgroundColor: '#f8f8f8',
+        borderBottomWidth: 2,
+        borderColor: '#eee',
+        justifyContent:'space-between'
+    },
+    listitemView: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    listitemText: {
+        fontSize: 18,
+        fontWeight:'bold',
+        color:'#424c58',
+    },
+    listitemID: {
+        fontSize: 12,
+        flex: 1
+    },
+    img:{
+        width: 32,
+        height: 30,
+        transform:[{rotate:'180deg'}],
+        backgroundColor:'#e57f01',
+        borderRadius:10,
+    },
+});
+
+export default DeviceInfo;
--- src/features/onBoarding/EmptyStateScreen.js
+++ src/features/onBoarding/EmptyStateScreen.js
@@ -1,34 +1,41 @@
 import React from 'react';
 import {
-    Platform,
+    Dimensions,
     StyleSheet,
     Text,
     View,
     StatusBar,
-    Image,
-    Button
+    Image
 } from 'react-native';
+import { Button } from '../../components';
 import navigation from '../../navigation';
 
 const EmptyState = (props) => {
     return (
         <View style={styles.container}>
             <StatusBar barStyle="light-content" backgroundColor="#555" />
-            <Image
-                source={require('../../assets/images/highrise-logo.png')}></Image>
-            <Text style={styles.welcome}>No Account Yet !</Text>
-            <Text style={styles.instructions}>
-                Add your device and see them here
-            </Text>
-            <Button
-                title="Connect Account"
-                onPress={() =>
-                    navigation.goTo(
-                        props.componentId,
-                        navigation.screenIds.addAccount
-                    )
-                }
-            />
+            <View style={{ flex: 0.4 }}>
+                <Image
+                    source={require('../../assets/images/addAccount.png')}
+                    style={{ width: 150, height: 150, marginTop: 40 }}></Image>
+            </View>
+            <View style={{ flex: 0.3 }}>
+                <Text style={styles.welcome}>No Account Yet !</Text>
+                <Text style={styles.instructions}>
+                    Add your device and see them here
+                </Text>
+                <View style={styles.bar}></View>
+                <Button
+                    title="Connect Account"
+                    style={styles.btnInvert}
+                    onPress={() =>
+                        navigation.goTo(
+                            props.componentId,
+                            navigation.screenIds.addAccount
+                        )
+                    }
+                />
+            </View>
         </View>
     );
 };
@@ -49,14 +56,37 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff'
     },
     welcome: {
-        fontSize: 20,
+        fontSize: 28,
         textAlign: 'center',
         margin: 10,
-        color: 'black'
+        color: 'black',
+        fontWeight: 'bold'
     },
     instructions: {
         textAlign: 'center',
-        color: 'maroon',
+        fontWeight: 'bold',
+        color: 'lightgrey',
         marginBottom: 5
+    },
+    btnInvert: {
+        backgroundColor: '#1c9db2',
+        borderRadius: 4,
+        paddingVertical: 25,
+        paddingHorizontal: 12,
+        fontSize: 14,
+        borderWidth: 0,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+        textTransform: 'uppercase',
+        width: Dimensions.get('window').width * 0.7
+    },
+    bar: {
+        alignSelf: 'center',
+        marginTop: 10,
+        marginBottom: 30,
+        padding: 10,
+        width: Dimensions.get('window').width * 0.5,
+        borderBottomColor: 'lightgrey',
+        borderBottomWidth: 1
     }
 });
