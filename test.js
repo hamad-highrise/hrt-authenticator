e3d1ea801b20fc0961ffd25f07f63e19980fb74b
Hamad Safdar <hamad@highrisetechnologies.com>
2021-02-24 12:49:33 +0500

    deleted extra folders

--- test.js
+++
@@ -1,9 +0,0 @@
-// import parser from './src/util/ui-parser';
-
-const parser = require('./src/util/ui-parser');
-
-const sample = `otpauth://totp/ACME%20Co:john.doe@email.com?secret=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&issuer=ACME%20Co&algorithm=SHA1&digits=6&period=30`;
-
-const result = parser(sample);
-
-console.log(result);
