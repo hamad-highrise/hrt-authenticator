c0886b625f7d6f341525dff349ec44f367451205
Hamad Safdar <hamad@highrisetechnologies.com>
2021-02-26 11:49:24 +0500

    Add account screens loop issue resolved

--- src/features/addAccount/AddAccountScreen.js
+++ src/features/addAccount/AddAccountScreen.js
@@ -5,7 +5,7 @@ import { Button, TopNavbar } from '../../components';
 import navigation from '../../navigation';
 const AddScreen = (props) => {
     const onQrScanClick = useCallback(() => {
-        navigation.goTo(props.componentId, navigation.screenIds.emptyState);
+        navigation.goTo(props.componentId, navigation.screenIds.qrScan);
     }, [props.componentId]);
 
     const onManualCodeClick = useCallback(() => {
