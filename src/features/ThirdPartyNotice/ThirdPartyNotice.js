6d7b63871b6fb668197fadb645b8662e5b24d680
RummanKhanHRT <rumman@highrisetechnologies.com>
2021-03-18 16:41:10 +0500

    stlying

--- src/features/ThirdPartyNotice/ThirdPartyNotice.js
+++ src/features/ThirdPartyNotice/ThirdPartyNotice.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { View, StyleSheet, Text, Image, Dimensions } from 'react-native';
+import {
+    View,
+    StyleSheet,
+    Text,
+    Image,
+    Dimensions,
+    ScrollView,
+    SafeAreaView,
+    TouchableOpacity
+} from 'react-native';
 import { Button } from '../../components';
 import PropTypes from 'prop-types';
 
@@ -16,27 +25,51 @@ const ThirdPartyNotice = () => {
                         fontWeight: 'bold',
                         color: '#424c58'
                     }}>
-                    Third Party Notice
+                    Privacy Policy
                 </Text>
             </View>
             <View style={styles.container}>
-                <Text style={styles.content}>
-                    This IBM Mobile Application Privacy Statement ("Mobile
-                    Privacy Statement") explains the data IBM may collect on
-                    behalf of the entity that entitles you to use this IBM
-                    product offering. This Mobile Privacy Statement only applies
-                    to the information IBM may collect on behalf of that entity.
-                    It does not apply to the information that entity may collect
-                    for its own use.
-                </Text>
-                <View style={{ margin: 20 }} />
-                <View>
-                    <Button
-                        title="Done"
-                        style={styles.btnInvert}
-                        onPress={() => alert('where to go ?')}
-                    />
-                </View>
+                <SafeAreaView>
+                    <ScrollView style={{ height:Dimensions.get('window').height * 0.75}}>
+                        <Text style={styles.content}>
+                            This IBM Mobile Application Privacy Statement ("Mobile
+                            Privacy Statement") explains the data IBM may collect on
+                            behalf of the entity that entitles you to use this IBM
+                            product offering. This Mobile Privacy Statement only
+                            applies to the information IBM may collect on behalf of
+                            that entity. It does not apply to the information that
+                            entity may collect for its own use.
+                        </Text>
+
+                        <Text style={styles.content}>
+                            Downloading, accessing, or otherwise using the App
+                            indicates that you have read this Mobile Privacy
+                            Statement and consent to its terms. If you do not
+                            consent to the terms of this Mobile Privacy Statement,
+                            do not proceed to download, access, or otherwise use the
+                            App.
+                        </Text>
+                        <Text style={styles.content}>
+                            IBM may collect the following information through the
+                            App:
+                            {"\n"}
+                            <Text>{'\u2022'}</Text> Personal information you may provide to download
+                            and use the App, including your email address, name, and
+                            password{"\n"}
+                            <Text>{'\u2022'}</Text> Information about your usage of the App,
+                            including crash logs and usage statistics
+                            <Text>{'\u2022'}</Text> Information about your device and its interaction with the App.
+                            including the type of mobile device you use with the App, its unique user ID, IP address, and operating system, and the type of mobile Internet browsers in use
+                        </Text>
+                    </ScrollView>
+                    
+                </SafeAreaView>
+                <View style={{ margin: 10 }} />
+                <TouchableOpacity
+                   onPress={() => alert('where to go ?')}
+                    style={styles.btnInvert}>
+                    <Text style={styles.label}>Done</Text>
+                </TouchableOpacity>
             </View>
         </View>
     );
@@ -46,7 +79,6 @@ export default ThirdPartyNotice;
 
 const styles = StyleSheet.create({
     container: {
-        // alignItems: 'center'
         margin: 25,
         marginTop: 10
     },
@@ -64,11 +96,8 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     btnInvert: {
-        backgroundColor: 'lightgrey',
         paddingVertical: 18,
         paddingHorizontal: 15,
-        borderWidth: 0,
-        borderRadius: 0,
         width: Dimensions.get('window').width * 0.25,
         alignSelf: 'flex-end'
     },
