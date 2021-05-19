bb9fa41888ffe3aed13a9796c7d52a5e5cecdae4
Hamad Safdar <hamad@highrisetechnologies.com>
2021-05-19 15:03:38 +0500

    basic redux setup

--- src/redux.js/index.js
+++ src/redux.js/index.js
@@ -1,22 +1,37 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
-import { alertReducer } from '../features/alert';
-import { mainReducer } from '../features/index.reducers';
+
+import { accountsReducer } from './accounts';
+import { utilsReducer } from './utils';
+
+// import { alertReducer } from '../features/alert';
+// import { mainReducer } from '../features/index.reducers';
 import thunk from 'redux-thunk';
-const rootReducer = combineReducers({ alert: alertReducer, main: mainReducer });
+// const rootReducer = combineReducers({ alert: alertReducer, main: mainReducer });
+const rootReducer = combineReducers({
+    accounts: accountsReducer,
+    utils: utilsReducer
+});
+
+// const initialState = {
+//     main: {
+//         selected: {},
+//         accounts: null
+//     },
+//     alert: {
+//         isConnected: null,
+//         loading: false,
+//         error: {
+//             hasOccurred: false,
+//             data: {}
+//         }
+//     }
+// };
 
 const initialState = {
-    main: {
-        selected: {},
-        accounts: null
-    },
-    alert: {
-        isConnected: null,
-        loading: false,
-        error: {
-            hasOccurred: false,
-            data: {}
-        }
-    }
+    accounts: [],
+    transaction: [],
+    errors: [],
+    utils: { loading: false, error: false, isConnected: null }
 };
 
 export default createStore(rootReducer, initialState, applyMiddleware(thunk));
