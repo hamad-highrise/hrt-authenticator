0e40b657dc108ec08255901c5d1852ab8d25e36d
Jahanzaib Sarwar <jahanzaib@Jahanzaibs-Mac-mini.local>
2021-11-11 16:51:21 +0500

    iOS Done

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ import { AppRegistry, NativeEventEmitter, Platform } from 'react-native';
 
 import App from './src/App';
 import { name as appName } from './app.json';
-import { utilities, push } from './src/native-services';
+import { utilities } from './src/native-services';
 
 //to prevent screenshot on Android Devices
 Platform.OS === 'android' && utilities.preventScreenshot();
-
-push.getFirebaseToken().then(console.log).catch(console.log);
 
 AppRegistry.registerComponent(appName, () => App);
